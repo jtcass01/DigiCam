ee52a690262e3e16b0e8c734f797e8655aaea3fb
Jacob Cassady <jacobtaylorcassady@outlook.com>
2023-07-11 06:41:00 -0400

    Updated comments

--- DigiCam/Camera.py
+++ DigiCam/Camera.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
-"""Camera.py: """
+"""
+Camera.py:
+
+This script encapsulates operations to control a DSLR camera using the DigiCamControl's command
+line interface. The script allows users to configure camera settings such as aperture, exposure
+control, shutter speed, and ISO through the Camera.Settings class.
+
+It supports the generation of XML-based setup scripts to configure the camera, execution of
+these scripts, and direct command interaction with the camera. Image capture functionality,
+both single and multiple, is provided, with optional autofocus control. Captured images can be
+saved to a specified directory, and are automatically given unique names based on a collection
+name and an incrementing image index.
+"""
 from __future__ import annotations
 
 __author__ = 'Jacob Taylor Cassady'
@@ -11,17 +23,25 @@ from typing import Union, IO, Optional, Dict
 
 
 class Camera:
-    """Camera class object.  Used to control a DSLR camera using digiCamControl's command line interface."""
+    """Camera class object.  Used to control a DSLR camera using digiCamControl's command line
+    interface."""
+
     def __init__(self, control_cmd_location: str, image_type: Optional[str] = None,
                  collection_name: str = '', save_folder: str = getcwd()):
         """Constructor.
 
         Args:
             control_cmd_location (str): The absolute or relative path to CameraControlCmd.exe.
-                                        If using a Windows OS is likely held within ProgramFiles\\digiCamControl\\.
-            image_type (Optional[str], optional): A string representing the image type to be captured.  Defaults to '.CR2' when None is passed.
-            collection_name (str, optional): A string to be appended to the front of ever image taken. Defaults to "".
-            save_folder (str, optional): The absolute or relative path to the directory where images are to be saved. Defaults to getcwd()."""
+            If using a Windows OS is likely held within ProgramFiles\\digiCamControl\\.
+
+            image_type (Optional[str], optional): A string representing the image type to be
+            captured.  Defaults to '.CR2' when None is passed.
+
+            collection_name (str, optional): A string to be appended to the front of ever image
+            taken. Defaults to ''.
+
+            save_folder (str, optional): The absolute or relative path to the directory where
+            images are to be saved. Defaults to getcwd()."""
         assert isfile(control_cmd_location), 'Unable to locate: ' + control_cmd_location \
             + '. Please ensure this is the correct path to CameraControlCmd.exe. ' \
             + 'It is likely held within Program Files\\digiCamControl\\'
@@ -37,8 +57,12 @@ class Camera:
         """Drives the setup of the camera given a set of settings.  Autocodes the setup script and runs it.
 
         Args:
-            settings (Camera.Settings): _description_
-            setup_script_name (str, optional): _description_. Defaults to 'setup.dccscript'."""
+            settings (Camera.Settings): Each setting from this object is
+            written to the provided file.
+
+            setup_script_name (str): represents the name of the setup script.
+            This script is used to configure the camera with the provided settings.
+            The script name defaults to 'setup.dccscript'."""
         self.generate_setup_script(settings=settings, setup_script_name=setup_script_name)
         self.run_script(script_name=setup_script_name)
 
@@ -46,8 +70,12 @@ class Camera:
         """Generates the setup script to set the aperture, exposure_control, shutter_speed, and iso of the camera if any of these values are passed.
 
         Args:
-            settings (Camera.Settings): _description_
-            setup_script_name (str): _description_"""
+            settings (Camera.Settings): Each setting from this object is
+            written to the provided file.
+
+            setup_script_name (str): represents the name of the setup script.
+            This script is used to configure the camera with the provided settings.
+            The script name defaults to 'setup.dccscript'."""
         # Generate the setup script at the script location with the given setup_script_name
         with open(setup_script_name, 'w+', encoding='utf-8') as file:
             file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
@@ -61,8 +89,10 @@ class Camera:
         """Writes the passed dictionary of settings to the passed file.  If a setting has a value of None, it is passed over.
 
         Args:
-            file (IO): [description]
-            settings (Camera.Settings): [description]"""
+            file (IO): Used to write the camera settings into the setup script.
+
+            settings (Camera.Settings): Each setting from this object is
+            written to the provided file."""
         # Write each setting in the settings dictionary to the file as long as the setting is not None
         for setting_name, setting in dict(settings).items():
             if setting is not None:
@@ -72,7 +102,8 @@ class Camera:
         """Creates a call to the camera using DigiCamControl
 
         Args:
-            command (str): [description]"""
+            command (str): represents a command that should be sent to the
+            camera using the DigiCamControl command line interface."""
         # Enforce directory location
         makedirs(self.save_folder, exist_ok=True)
 
@@ -85,7 +116,7 @@ class Camera:
         """Runs the passed script within the script location.
 
         Args:
-            script_name (str): [description]"""
+            script_name (str): The name of the script that should be run to configure the camera."""
         # Make call to operating system
         system(f'{self.control_cmd_location} {script_name}')
 
@@ -94,10 +125,14 @@ class Camera:
         """Sets the image type.  If none is given, the default .jpg is used.
 
         Args:
-            image_type (Union[str, None], optional): [description]. Defaults to None.
+            image_type (Union[str, None], optional):  string that represents
+            the type of image to be captured (for example, 'jpg', 'raw',
+            'png', '.CR2'). If no image type is provided, the default is
+            'jpg'. Defaults to None.
 
         Returns:
-            str: A string representing the image type.  If none is given, the default .jpg is used."""
+            str: A string representing the image type.  If none is given, the
+            default .jpg is used."""
         if image_type in ['jpeg', 'jpg']:
             return '.jpg'
 
@@ -113,10 +148,13 @@ class Camera:
         return '.jpg'
 
     def capture_single_image(self, autofocus: bool = False) -> None:
-        """Captures a single image.  Iterates the image index to ensure a unique name for each image taken.
+        """Captures a single image.  Iterates the image index to ensure a unique
+        name for each image taken.
 
         Args:
-            autofocus (bool, optional): [description]. Defaults to False."""
+            autofocus (bool, optional): Determines whether autofocus should be
+                enabled when capturing the image. The default is False, meaning
+                autofocus is not enabled unless specified. Defaults to False."""
         # Make the correct camera command depending on autofocus being enabled.
         if autofocus:
             self.command_camera('/capture')
@@ -126,16 +164,20 @@ class Camera:
         # Increment the image index
         self.image_index += 1
 
-    def capture_multiple_images(self, image_count):
+    def capture_multiple_images(self, image_count: int):
         """Captures an n number of images by repeatedly calling the capture_single_image function n times where n is the parameter image_count.
 
         Args:
-            image_count ([type]): [description]"""
+            image_count (int): Specifies the number of images to be captured."""
         # Iterate over the image_count capturing a single image every time.
         for _ in range(image_count):
             self.capture_single_image()
 
     class Settings:
+        """A helper class that holds various camera settings, such as aperture,
+        exposure control, shutter speed, and ISO. These settings are used to
+        configure the camera before capturing images."""
+
         def __init__(self, aperture: Optional[str] = None,
                      exposure_control: Optional[str] = None,
                      shutter_speed: Optional[str] = None,
